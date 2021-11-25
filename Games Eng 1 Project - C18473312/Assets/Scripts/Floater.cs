58626d6810b13b56372ecc535abcf422107ca2af
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2021-11-25 22:57:20 +0000

    Floater tutorial complete, will need a whole rewrite, onto sea generation now

--- Games Eng 1 Project - C18473312/Assets/Scripts/Floater.cs
+++ Games Eng 1 Project - C18473312/Assets/Scripts/Floater.cs
@@ -7,6 +7,10 @@ public class Floater : MonoBehaviour
     public Rigidbody rigidBody;
     public float depthBeforeSubmerged = 1f;
     public float displacementAmmount = 3f;
+    public float waterDrag = .99f;
+    public float waterAngularDrag = .5f;
+    public int floaterCount = 1;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +21,17 @@ public class Floater : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate(){
 
+      //Applying gravity on the floaters
+      rigidBody.AddForceAtPosition(Physics.gravity/floaterCount,transform.position, ForceMode.Acceleration);
+
       // Fetch wave manager instance value
       float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
 
       if(transform.position.y < waveHeight){
         float displacementMultiplier = Mathf.Clamp01((waveHeight-transform.position.y)/depthBeforeSubmerged)*displacementAmmount;
-        rigidBody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) *displacementMultiplier, 0f), ForceMode.Acceleration);
+        rigidBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) *displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+        rigidBody.AddForce(displacementMultiplier*-rigidBody.velocity*waterDrag *Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rigidBody.AddTorque(displacementMultiplier*-rigidBody.angularVelocity*waterAngularDrag *Time.fixedDeltaTime, ForceMode.VelocityChange);
       }
     }
 }

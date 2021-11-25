0f11f97657d3faf2174385584224771b5d42bb31
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2021-11-25 23:22:45 +0000

    Imported perlin generator from lab/lecture, re-engineering to allow for perlin noise sea and boats

---
+++ Games Eng 1 Project - C18473312/Assets/Scripts/WaterHeightCalculator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterHeightAtPoint : MonoBehaviour
+{
+
+    // Singleton instance
+    public static WaveManager instance;
+
+    // Wave Parametres
+    public float aplitude = 1f;
+    public float length = 2f;
+    public float speed = 1f;
+    public float offset = 0f;
+
+    private void Update(){
+        offset += Time.deltaTime * speed;
+    }
+
+    // For Given x value, will need to be changed for Perlin's X/Y axis
+    public float GetWaveHeight(float _x){
+        return aplitude * Mathf.Sin(_x/length + offset);
+    }
+
+
+
+    // Singleton code
+    private void Awake(){
+        if(instance == null){
+            instance = this;
+        }
+        else if(instance != this){
+            Debug.Log("Wavemanager instance already running, destroying duplicate instance.");
+            Destroy(this);
+        }
+    }
+}

b98f0bb6c2720a28e99b9363bb6f9f1428b365c1
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2021-12-08 12:56:28 +0000

    Saving, sea noise and chunkdata generator complete

---
+++ Games Eng 1 Project - C18473312/Assets/Scripts/Sea/ChunkGeneration/Noise Generation/SeaNoiseSource.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used as the source of sea waves
+public static class SeaNoiseSource {
+    
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, Vector2 offset){
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+        
+        // Vector given is in the centre of noisemap.
+        float halfWidth = mapWidth/2;
+        float halfHeight = mapHeight/2;
+
+
+        //Height Sampling Loop
+        for(int y = 0; y < mapHeight; y++){
+            for(int x = 0; x < mapWidth; x++){
+
+                // No Octaves since we want smooth noise.
+                float sample_x = (x-halfWidth + offset.x)/scale;
+                float sample_y = (y+halfHeight  - offset.y)/scale;
+
+                noiseMap[x,y] = Mathf.PerlinNoise(sample_x,sample_y);
+            }
+        }
+        return noiseMap;
+    }
+
+    // WILLL NEED TO BE TESTED TO SEE IF ITS WORKING
+    public static float GetHeightAtPoint(float scale, Vector2 offset){
+        return Mathf.PerlinNoise(offset.x/scale, offset.y/scale);
+    } 
+
+
+}    
---
+++ Games Eng 1 Project - C18473312/Assets/Scripts/Sea/ChunkGeneration/SeaChunkDataController.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class SeaChunkDataController : MonoBehaviour
+{
+    // Constant Variables
+    public const int chunkSize = 241; // 241 vertices, making a 240 length piece.
+
+    // Configurable Variables
+    public float waveFrequency; // How much zoom into the noise map.
+    
+    public Vector2 offset; // Input offset.
+    public Vector2 offSetSpeed; // Ammount waves change.
+
+    public float waveHeightMultiplier; // amplitude
+
+
+    // keeping for later might experiement
+    public AnimationCurve meshHeightCurve; // Curve perlin input. Exactly like using colour curves for editing photos.
+
+    // Also keeping for experimentation
+    public TerrainLayer[] terrainLayers; // Used to colour map
+
+    [System.Serializable] // Makes Struct Visible to unity
+    public struct TerrainLayer {
+        public string name;
+        public float height;
+        public Color colour;
+    }
+
+
+    // ================== Core Functionality Code =========================
+    ChunkData GenerateChunkDataForPoint(Vector2 centre){
+        float[,] noiseMap = SeaNoiseSource.GenerateNoiseMap(chunkSize, chunkSize,  waveFrequency, centre+offset);
+        //float[,] noiseMap = Noise.GenerateNoiseMap(chunkSize, chunkSize, seed, noiseScale, octaves, peristance, lacunarity, centre+offset, Noise.NormalizeMode.Global);
+        Color[] colourMap = new Color[chunkSize*chunkSize]; // Colours need to be stored in a 1D array;
+        
+        for(int y = 0; y < chunkSize; y++){
+            for(int x = 0; x < chunkSize; x++){
+                float currentHeight = noiseMap[x,y];
+
+                // Cycle through all terrain height layers
+                for(int i = 0; i < terrainLayers.Length; i++){
+                    
+                    if(currentHeight >= terrainLayers[i].height){
+                        colourMap[y*chunkSize+ x] = terrainLayers[i].colour;
+                    } 
+                }
+            }
+        }
+
+        // Output is a map of values to create a mesh and texture from.
+        return new ChunkData(noiseMap,colourMap);
+    }
+
+
+    // Will be used by floaters
+    float GetHeightAtPoint(Vector2 centre){
+        return waveHeightMultiplier*SeaNoiseSource.GetHeightAtPoint(chunkSize,  waveFrequency, centre+offset);
+    }
+
+
+    // ================== Multithreading code ============================
+
+    // Thread Queues
+    Queue<DataCallbackPair<ChunkData>> chunkDataThreadOutputQueue = new Queue<DataCallbackPair<ChunkData>>(); 
+    Queue<DataCallbackPair<MeshData>> meshDataThreadOutputQueue = new Queue<DataCallbackPair<MeshData>>();
+
+    public struct DataCallbackPair<T>{
+        public readonly Action<T> callback;
+        public readonly T data;
+
+        public DataCallbackPair(Action<T> callback, T data){
+            this.callback = callback;
+            this.data = data;
+        }
+    }
+
+
+    // === Request Data on Thread Calls
+    /*
+        1. Requests are used to instanciate a thread.
+        2. Threads complete work and add to queues.
+        3. Main thread fetches data off of queue and sends in callback.
+    */
+    public void RequestChunkData(Vector2 centre, Action<ChunkData> callback){
+        ThreadStart threadStart = delegate{
+            ChunkDataRequestThread(centre, callback);
+        };
+        new Thread(threadStart).Start();
+    }
+    void ChunkDataRequestThread(Vector2 centre, Action<ChunkData> callback){
+        ChunkData chunkData = GenerateChunkDataForPoint(centre);
+
+        lock(chunkDataThreadOutputQueue){
+            chunkDataThreadOutputQueue.Enqueue(new DataCallbackPair<ChunkData>(callback, chunkData));
+        }
+    }
+    
+    public void RequestMeshData(ChunkData mapData, int lod, Action<MeshData> callback){
+        ThreadStart threadStart = delegate{
+            MeshDataThread(mapData, lod,  callback);
+        };
+        new Thread(threadStart).Start();
+    }
+    void MeshDataThread(ChunkData mapData,int lod, Action<MeshData> callback){
+        MeshData meshData = MeshGenerator.GenerateTerrainMesh(mapData.heightMap, heightMultiplier, meshHeightCurve, lod);
+
+        lock(meshDataThreadOutputQueue){
+            meshDataThreadOutputQueue.Enqueue(new DataCallbackPair<MeshData>(callback, meshData));
+        }
+    
+    }
+
+    // For the main thread to call
+    void CheckThreadQueues(){
+        if(chunkDataThreadOutputQueue.Count > 0){
+            for(int i = 0; i < chunkDataThreadOutputQueue.Count; i++){
+                DataCallbackPair<ChunkData> threadOutput = chunkDataThreadOutputQueue.Dequeue();
+                threadOutput.callback(threadOutput.data);
+            }
+        }
+        if(meshDataThreadOutputQueue.Count > 0){
+            for(int i = 0; i < meshDataThreadOutputQueue.Count; i++){
+                DataCallbackPair<MeshData> threadOutput = meshDataThreadOutputQueue.Dequeue();
+                threadOutput.callback(threadOutput.data);
+            }
+        }
+
+    }
+    // ============================================================================================
+
+    void Update(){
+        offset -+ offsetSpeed * Time.deltaTime;
+
+        CheckThreadQueues();
+    }
+
+    // ================== Validations of Input code ============================
+    void OnValidate() {
+        if(lacunarity < 1){
+            lacunarity = 1;
+        }
+        if(octaves < 1){
+            octaves = 1;
+        }
+
+        if(octaves < 0){
+            octaves = 0;
+        }
+        if(noiseScale <=0){
+            noiseScale = 0.0001f;
+        }
+
+    }
+}

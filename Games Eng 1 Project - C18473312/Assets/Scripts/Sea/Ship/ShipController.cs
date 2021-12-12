ef135e574e078b2e773f1fe2f3883e23b8e2ea16
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2021-12-12 11:43:52 +0000

    Forward driving implemented, need to implent turning now

--- Games Eng 1 Project - C18473312/Assets/Scripts/Sea/Ship/ShipController.cs
+++ Games Eng 1 Project - C18473312/Assets/Scripts/Sea/Ship/ShipController.cs
@@ -25,14 +25,16 @@ public class ShipController : MonoBehaviour{
 
     private float wheelRotation = 0;
     private float sailState = 0f;
-    private float sailSize = 20; // How long extended sails are
+    private float maxSailSize = 20f; // How long extended sails are
+    private float minSailSize = 2f; // how long retracted sails are.
 
     // ================= Lifecycle methods =================================
     void Start() {
         steeringWheel = GameObject.FindWithTag("Ship Steering Wheel"); // I tagged the steering wheel to access for rotation.
         sails = GameObject.FindWithTag("Ship Sails"); // same for sails
-
-        Debug.Log(sails);
+       
+        // Getting the ship's rigidbody.
+        shipRigidbody = this.gameObject.GetComponent<Rigidbody>();
     }
 
 
@@ -54,23 +56,50 @@ public class ShipController : MonoBehaviour{
         steeringWheel.transform.localRotation = Quaternion.Euler(0, 0, wheelRotation);
 
         // Set sail state
-        sailState = Mathf.Clamp(sailState, 2f, sailSize);
+        sailState = Mathf.Clamp(sailState, minSailSize, maxSailSize);
         sails.transform.localScale = new Vector3(25f, sailState, 1); // rezise sails
         sails.transform.localPosition = new Vector3(-1, -sailState/2 + 30, 2.68f); // adjust placement of sails to scale.
 
 
+
+
+        ApplyShipDriving();
         // Ship physics method will go here =========
     }
 
     // ===================================================================
 
-    // Used to dictate if component is being used.
+    // Used to dictate if component is being steered.
     public void SteerShip(bool steering) {
         this.enabled = steering;
         objectInterractionComponent.enabled = !steering;
         characterController.enabled = !steering;
         characterControllerScript.enabled = !steering;
     }
+
+
+    // Applies vehicle control forces to rigidbody;
+    private void ApplyShipDriving() {
+
+        // 1. Add forward force based on speed / sail ratio.
+
+        // firstly we get the forward vector of the ship.
+        Vector3 forwardForce = shipRigidbody.gameObject.transform.forward;
+
+        // This multiplies the forward vector by ship speed / ammount of sails used.
+        forwardForce = forwardForce * shipSpeed * Mathf.InverseLerp(minSailSize, maxSailSize, sailState);
+
+        shipRigidbody.AddForce(forwardForce, ForceMode.VelocityChange); // Apply force
+
+
+        // 2. Ship rotation.
+
+
+        // if sail ratio 0 don't apply rotation, else apply rotation per wheel ratio
+        //rigidBody.AddTorque(upPullStrength*-rigidBody.angularVelocity*waterAngularDrag *Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+
+
 
 
 
